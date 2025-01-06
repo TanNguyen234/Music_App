ea7e5a1c984b9969d968e1aa05500c02b6926312
TanNguyen234 <tanntd.2005@gmail.com>
2025-01-06 18:29:46 +0700

    login

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -6,3 +6,19 @@ function loadDelayed() {
   // E.g. trigger via timeout after 5 seconds
   setTimeout(loadDelayed, 5000);
 //End Lazy loading
+const inputs = document.querySelectorAll("input");
+window.addEventListener('DOMContentLoaded', () => {
+  if(inputs.length > 0) {
+    inputs.forEach(input => {
+      const attribute = input.getAttribute('id')
+      input.addEventListener('focus', () => {
+        input.setAttribute('type', attribute);
+        // input.addEventListener('invalid', () => {
+        //   // Đảm bảo rằng thông báo lỗi được cập nhật khi trường không hợp lệ
+        //   input.setCustomValidity("Vui lòng nhập " + attribute);
+        // });
+      })
+    });
+  }
+});
+//End Chặn auto fill
